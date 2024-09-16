4352d31ff71491bb92ec42df2438236e88122149
Ardek <cheethamthing@gmail.com>
2024-09-16 20:07:40 +0100

    Feat: map gen improved

--- src/client/game/map.js
+++ src/client/game/map.js
@@ -9,7 +9,7 @@ H.S.S.S.S.S.S.H
 H.............H
 H.S.S.S.S.S.S.H
 H.............H
-H.S.S.S.S.S.S.H
+H.S.S.S.S.H.S.H
 H.............H
 H.S.S.S.S.S.S.H
 H.............H
@@ -18,6 +18,13 @@ H.............H
 HHHHHHHHHHHHHHH
 `;
 
+const levelChars = {
+    "+": "banner",
+    "H": "hard-block",
+    "S": "soft-block",
+    ".": "ground",
+    "@": "player",
+}
 // Function to create the gameMap element
 export const gameMap = () => {
 
@@ -25,31 +32,9 @@ export const gameMap = () => {
     let rows = map.trim().split("\n").map((line) => [...line]);
 
     // Generate the map elements based on the symbols
-    const mapElements = rows.flatMap((row) => 
+    const mapElements = rows.flatMap((row) =>
         row.map((cell) => {
-            let className;
-
-            // Determine the class based on the cell symbol
-            switch (cell) {
-                case 'H':  // Hard Block/Wall
-                    className = "hard-block";
-                    break;
-                case 'S':  // Soft Block/Destructible Wall
-                    className = "soft-block";
-                    break;
-                case '+':  // Banner
-                    className = "banner";
-                    break;
-                case '.':  // Empty space
-                    className = "ground";
-                    break;
-                case '@':  // Player (if placed in map)
-                    className = "player";
-                    break;
-                default:
-                    className = "unknown";  // Fallback for unknown symbols
-                    break;
-            }
+            let className = levelChars[cell]
 
             // Create the individual cell
             return createElement("div", {
