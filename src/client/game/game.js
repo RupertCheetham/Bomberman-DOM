2fbe9785a6b257cc67da6a6b695a6b5e8c78b6ea
Ardek <cheethamthing@gmail.com>
2024-10-23 16:53:53 +0100

    Refactor, blanked out currently unused function

--- src/client/game/game.js
+++ src/client/game/game.js
@@ -127,15 +127,15 @@ const endGame = () => {
 //   startGameTimer(gameDuration);
 // };
 
-export const createLivesDisplay = (player) => {
-  const livesElement = createElement("div", {
-    attrs: {
-      class: `${player.id}-lives`, // Unique class for each player's lives display
-      style: "margin: 10px;", // You can customize the style as needed
-    },
-    children: [`Player ${player.id} Lives: ${player.lives}`], // Display initial lives
-  });
-};
+// export const createLivesDisplay = (player) => {
+//   const livesElement = createElement("div", {
+//     attrs: {
+//       class: `${player.id}-lives`, // Unique class for each player's lives display
+//       style: "margin: 10px;", // You can customize the style as needed
+//     },
+//     children: [`Player ${player.id} Lives: ${player.lives}`], // Display initial lives
+//   });
+// };
 
 // Function to create and spawn players
 export const spawnPlayers = (playerNum) => {
